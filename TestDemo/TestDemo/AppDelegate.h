387e7c81bc9f9f2aa370f19a80a1bda3b92c0455
闫博 <xiaobo@yanbodeMac-mini.local>
2024-12-30 18:51:18 +0800

    新增项目

---
+++ TestDemo/TestDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  TestDemo
+//
+//  Created by 闫博 on 2024/12/30.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ TestDemo/TestDemo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  TestDemo
+//
+//  Created by 闫博 on 2024/12/30.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
